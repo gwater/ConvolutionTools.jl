eda53b0c9542e8556fd47c4349e419f8d0d2b0be
Josua Grawitter <josh@greyage.org>
2016-03-04 19:10:59 +0100

    add initial code

--- src/ConvolutionTools.jl
+++ src/ConvolutionTools.jl
@@ -1,5 +1,33 @@
 module ConvolutionTools
 
-# package code goes here
+import Base.conv
+
+export conv
+
+function conv{T<:Base.LinAlg.BlasFloat, N}(u::Array{T, N}, v::Array{T, N})
+    su = [size(u)...]
+    sv = [size(v)...]
+    sr = abs(su .- sv) .+ 1 # for cropping valid parts
+    sp = su .+ sv .- 1      # for zero-padding
+    # pad for efficient FFT
+    sp2 = [n > 1024 ? nextprod([2,3,5], n) : nextpow2(n) for n in sp]
+    upad = zeros(T, (sp2...))
+    upad[[1:n for n in su]...] = u
+    vpad = zeros(T, (sp2...))
+    vpad[[1:n for n in sv]...] = v
+    if T <: Real
+        p = plan_rfft(upad)
+    else
+        p = plan_fft(upad)
+    end
+    # perform convolution
+    y = p \ ((p * upad) .* (p * vpad))
+    # only return valid part
+    invalid = div(sp .- sr,  2)
+    lower = invalid .+ 1
+    upper = invalid .+ sr
+    center = [lower[i]:upper[i] for i in 1:N]
+    return y[center...]
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,72 @@
 using ConvolutionTools
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+import Base.isapprox
+
+function isapprox{T, N}(a::Array{T, N}, b::Array{T, N}, eps=10e-10)
+    if size(a) != size(b)
+        return false
+    end
+    el_diff = sum(abs(a .- b)) / prod(size(a))
+    return el_diff < eps
+end
+
+function test_1d()
+    a = rand((2))
+    b = rand((7))
+    res = conv(a, b)
+    return ndims(res) == 1 && eltype(res) <: Number
+end
+
+function test_2d()
+    a = rand((1, 2))
+    b = rand((6, 7))
+    res = conv(a, b)
+    return ndims(res) == 2 && eltype(res) <: Number
+end
+
+function test_3d()
+    a = rand((1, 2, 3))
+    b = rand((6, 7, 10))
+    res = conv(a, b)
+    return ndims(res) == 3 && eltype(res) <: Number
+end
+
+function test_identity()
+    a = float([1])
+    b = float([i^2 for i in 1:100])
+    res = conv(a, b)
+    return isapprox(b, res)
+end
+
+function test_zero()
+    a = float([0])
+    b = float([i^2 for i in 1:100])
+    res = conv(a, b)
+    return all(res .== zero(eltype(res)))
+end
+
+function test_derivative()
+    n = 10
+    a = float([1, 0, -1]) / 2
+    b = float([0.5 * i^2 for i in 1:n])
+    res = conv(a, b)
+    ref = float([i for i in 2:(n-1)])
+    return isapprox(ref, res)
+end
+
+custom_handler(r::Test.Success) = print(".")
+custom_handler(r::Test.Failure) = Test.default_handler(r)
+custom_handler(r::Test.Error)   = Test.default_handler(r)
+
+Test.with_handler(custom_handler) do
+    @test test_1d()
+    @test test_2d()
+    @test test_3d()
+    @test test_zero()
+    @test test_identity()
+    @test test_derivative()
+end
+
+println("")
+
